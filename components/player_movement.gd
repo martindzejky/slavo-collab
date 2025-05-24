b5527b34f81e2afb01d1b640d33c97e7fde51300
Martin Jakubik <martindzejky@gmail.com>
2025-05-24 14:18:58 +0200

    physics movement

--- components/player_movement.gd
+++ components/player_movement.gd
@@ -1,6 +1,10 @@
 extends Movement
 class_name PlayerMovement
 
-func _process(delta: float) -> void:
+@export var root: CharacterBody2D
+
+func _physics_process(delta: float) -> void:
   move_direction = Input.get_vector('move_left', 'move_right', 'move_up', 'move_down')
-  root.position += move_direction * move_speed * delta
+
+  root.velocity = move_direction * move_speed
+  root.move_and_slide()
